fb43ab8151ea827d636eeacbcfe5bb053269a118
C1rF <cyaoad@gmail.com>
2022-09-29 22:42:40 +0800

    Changed VisualNeoController setApp

--- src/main/java/hkust/edu/visualneo/VisualNeoApp.java
+++ src/main/java/hkust/edu/visualneo/VisualNeoApp.java
@@ -20,11 +20,11 @@ public class VisualNeoApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(VisualNeoApp.class.getResource("fxml/visualneo-home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        VisualNeoController controller = fxmlLoader.<VisualNeoController>getController();
+        controller.setApp(this);
         stage.setTitle("VisualNeo");
         stage.setScene(scene);
         stage.show();
-
-        queryHandler = new QueryHandler(this);
     }
 
     public static void main(String[] args) {
