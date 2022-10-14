7ace6b44d98829a9c67d46626b3e66c530b6c306
Houdong <houdongliang@HoudongdeMacBook-Pro.local>
2022-10-14 23:02:35 +0800

    Added the load animation function and handle all the exceptions

--- src/main/java/hkust/edu/visualneo/LoadDatabaseController.java
+++ src/main/java/hkust/edu/visualneo/LoadDatabaseController.java
@@ -1,10 +1,13 @@
 package hkust.edu.visualneo;
 
+import hkust.edu.visualneo.utils.frontend.LoadAnimation;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.neo4j.driver.exceptions.AuthenticationException;
 
 public class LoadDatabaseController {
 
@@ -24,6 +27,9 @@ public class LoadDatabaseController {
     @FXML
     private Button btn_load;
 
+    // Boolean variable to indicate whether connection is successful
+    Boolean connect_success;
+
     @FXML
     void handleCancel() {
         Stage stage = (Stage) btn_cancel.getScene().getWindow();
@@ -32,22 +38,66 @@ public class LoadDatabaseController {
 
     @FXML
     void handleLoad() {
+
+        // Create the animation
+        Stage stage = (Stage) btn_load.getScene().getWindow();
+        LoadAnimation loadAnimation = new LoadAnimation(stage);
+
+        // Get the user's input
         String uri = textfield_uri.getText();
         String user = textfield_user.getText();
         String password = textfield_password.getText();
-        try{
-            controller.submitDBInfo(uri, user, password);
-            // If the database is successfully connected, close the window
-            handleCancel();
-            // update the UI with the database meta info
-            controller.updateUIWithMetaInfo();
-        }catch (Exception e){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Database Connection Error");
-            alert.setHeaderText("Cannot connect the database. ");
-            alert.setContentText("The provided information is not correct!");
-            alert.showAndWait();
-        }
+
+        //worker thread for connecting the database
+        Thread worker = new Thread(new Runnable(){
+            public void run(){
+                try{
+                    // Try to connect to the database
+                    controller.submitDBInfo(uri, user, password);
+                    //controller.submitDBInfo("bolt://44.201.105.181:7687", "neo4j", "scissors-capstan-diseases");
+                    connect_success = true;
+                }catch (AuthenticationException e){
+                    connect_success = false;
+                    Platform.runLater(()->{
+                        System.out.println(e.getMessage());
+                        loadAnimation.cancelLoadAnimation();
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Database Connection Error");
+                        alert.setHeaderText("Cannot connect the database. ");
+                        alert.setContentText("The provided information is not correct!");
+                        alert.showAndWait();
+                    });
+                }
+            }
+        });
+        worker.start();
+
+        // Load Animation Starts
+        loadAnimation.activateLoadAnimation();
+
+        //observer thread for notifications
+        new Thread(new Runnable(){
+            public void run(){
+                try{
+                    // Try to connect to the database
+                    worker.join();
+                }
+                catch(Exception e){;}
+                finally{
+                    Platform.runLater(()->{
+                        System.out.println("The load database thread ends");
+                        if(connect_success){
+                            loadAnimation.cancelLoadAnimation();
+                            // If the database is successfully connected, close the window
+                            handleCancel();
+                            // update the UI with the database meta info
+                            controller.updateUIWithMetaInfo();
+                        }
+                    });
+                }
+            }
+        }).start();
+
     }
 
     public void setVisualNeoController(VisualNeoController controller){
---
+++ src/main/java/hkust/edu/visualneo/utils/frontend/LoadAnimation.java
@@ -0,0 +1,86 @@
+package hkust.edu.visualneo.utils.frontend;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import java.io.IOException;
+import static javafx.animation.Animation.INDEFINITE;
+
+
+public class LoadAnimation {
+
+    private Stage dialogStage;
+    private SequentialTransition whole_animation;
+    private final int NUM_LABELS = 3;
+
+    public LoadAnimation(Stage primaryStage){
+
+        dialogStage = new Stage();
+
+        // Set the parent-child relationship
+        dialogStage.initOwner(primaryStage);
+        dialogStage.initStyle(StageStyle.UNDECORATED);
+        dialogStage.initStyle(StageStyle.TRANSPARENT);
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, primaryStage.getWidth(), primaryStage.getHeight());
+        pane.setStyle("-fx-background-color: rgba(0, 0, 0, 0.1);");
+        scene.setFill(null);
+        dialogStage.setScene(scene);
+
+        String[] labels_content = {"Load Database .", "Load Database ..", "Load Database ..."};
+        Label[] labels = new Label[NUM_LABELS];
+        FadeTransition[] fade_all = new FadeTransition[NUM_LABELS];
+        FadeTransition[] show_all = new FadeTransition[NUM_LABELS];
+        TranslateTransition[] delays = new TranslateTransition[NUM_LABELS];
+
+        for(int i=0; i<NUM_LABELS; i++){
+            // Set the labels
+            labels[i] = new Label(labels_content[i]);
+            labels[i].setFont(Font.font("Courier", 20));
+            labels[i].setTextFill(Color.BLUE);
+            labels[i].layoutXProperty().bind(pane.widthProperty().subtract(labels[i].widthProperty()).divide(2));
+            labels[i].layoutYProperty().bind(pane.heightProperty().subtract(labels[i].heightProperty()).divide(2));
+            pane.getChildren().add(labels[i]);
+            // Set the animation
+            fade_all[i] = new FadeTransition(Duration.millis(1),labels[i]);
+            show_all[i] = new FadeTransition(Duration.millis(1),labels[i]);
+            delays[i] = new TranslateTransition(Duration.millis(999));
+            //setting the initial and the target opacity value for the transition
+            fade_all[i].setFromValue(1);fade_all[i].setToValue(0);
+            show_all[i].setFromValue(0);show_all[i].setToValue(1);
+        }
+        whole_animation = new SequentialTransition (
+                delays[0],fade_all[0],show_all[1],
+                delays[1],fade_all[1],show_all[2],
+                delays[2],fade_all[2],show_all[0]
+        );
+        whole_animation.setCycleCount(INDEFINITE);
+    }
+
+    public void activateLoadAnimation() {
+        System.out.println("Start the animation");
+        dialogStage.show();
+        whole_animation.play();
+    }
+
+    public Stage getDialogStage(){
+        return dialogStage;
+    }
+
+    public void cancelLoadAnimation() {
+        System.out.println("Stop the animation");
+        dialogStage.close();
+        whole_animation.stop();
+    }
+}
