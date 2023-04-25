dd326d502e7309e5dbae1995b9395da5f453451f
C1rF <cyaoad@gmail.com>
2023-04-25 20:23:08 +0800

    Fixed a display bug when querying a single node

--- src/main/java/hkust/edu/visualneo/utils/backend/QueryBuilder.java
+++ src/main/java/hkust/edu/visualneo/utils/backend/QueryBuilder.java
@@ -19,7 +19,6 @@ public class QueryBuilder {
 
     private final StringProperty translation = new SimpleStringProperty(this, "translation", null);
 
-    // TODO: Modify this
     public String translate(Graph graph, boolean simple) {
         if (graph.isEmpty())
             throw new Graph.BadTopologyException(Graph.BadTopologyException.TopologyType.EMPTY);
@@ -48,14 +47,16 @@ public class QueryBuilder {
         buffer.append(keywordSeparator);
 
         Iterator<Relation> relationIt = graph.getRelations().iterator();
-        while (true) {
-            Relation relation = relationIt.next();
+        Relation relation = relationIt.next();
+        translate(relation.start, unusedNodes);
+        translate(relation);
+        translate(relation.end, unusedNodes);
+        while (relationIt.hasNext()) {
+            buffer.append(commaSeparator);
+            relation = relationIt.next();
             translate(relation.start, unusedNodes);
             translate(relation);
             translate(relation.end, unusedNodes);
-            if (!relationIt.hasNext())
-                break;
-            buffer.append(commaSeparator);
         }
 
         buffer.append(NEW_LINE);
@@ -141,14 +142,6 @@ public class QueryBuilder {
         catch (Graph.BadTopologyException e) {
             setTranslation("");
         }
-    }
-
-    public void unbind() {
-        setTranslation(null);
-    }
-
-    public boolean isBound() {
-        return getTranslation() == null;
     }
 
     public StringProperty translationProperty() {
